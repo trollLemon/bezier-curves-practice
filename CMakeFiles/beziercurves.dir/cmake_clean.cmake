131890315f6e3ca64fb3b585aedb1cbeb5493f13
trolllemon <haydnt@zohomail.com>
2022-07-24 11:19:43 -0700

    initialized sld2

---
+++ CMakeFiles/beziercurves.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/beziercurves.dir/src/main.cpp.o"
+  "CMakeFiles/beziercurves.dir/src/main.cpp.o.d"
+  "beziercurves"
+  "beziercurves.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/beziercurves.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
